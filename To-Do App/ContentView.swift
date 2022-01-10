81ec1c454ee7e8b77f6e4cc3bdf63907a60ee687
urosbmx <urosbmx@gmail.com>
2022-01-10 21:25:35 +0100

    Initial Commit

---
+++ To-Do App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To-Do App
+//
+//  Created by Uroš Katanić on 10.1.22..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ To-Do App/To_Do_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_AppApp.swift
+//  To-Do App
+//
+//  Created by Uroš Katanić on 10.1.22..
+//
+
+import SwiftUI
+
+@main
+struct To_Do_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
